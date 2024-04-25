b2b78bc6275b1de7d3f4d170a0fdc1b351c797c4
letsmovetocloud <88219893+letsmovetocloud@users.noreply.github.com>
2024-04-25 11:34:24 +0530

    vpc details updated

--- ec2.tf
+++ ec2.tf
@@ -29,9 +29,9 @@ module "vpc" {
 
   # VPC Basic Details
   name = "vpc-dev"
-  cidr = "10.0.0.0/16"   
+  cidr = "10.5.0.0/16"   
   azs                 = ["us-east-1a"]
-  private_subnets     = ["10.0.1.0/24"]
-  public_subnets      = ["10.0.101.0/24"]
+  private_subnets     = ["10.5.1.0/24"]
+  public_subnets      = ["10.5.101.0/24"]
 
 #new comment added 
